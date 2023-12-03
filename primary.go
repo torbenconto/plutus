6f80a3f0ee14c83e280a8e3e119b7ddae114237e
torbenconto <torben@2x1.dev>
2023-12-03 16:11:47 -0500

    fix helper func

--- primary.go
+++ primary.go
@@ -2,8 +2,5 @@ package main
 
 // Helper function to check if the provided attribute belongs to the primary stock on the page
 func isPrimary(attr string) bool {
-	if attr == "" {
-		return true
-	}
-	return false
+	return attr == ""
 }
