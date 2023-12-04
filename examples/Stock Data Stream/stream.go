7771760af652196e5ba2148ddaf9e7f4fe832fd2
torbenconto <torben@2x1.dev>
2023-12-03 19:16:11 -0500

    example of using data stream

---
+++ examples/Stock Data Stream/stream.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/torbenconto/plutus"
+)
+
+func main() {
+	// Create new Stock object
+	stock, err := plutus.NewStock("AMD")
+	if err != nil {
+		fmt.Println("Error fetching data for stock")
+	}
+
+	// Set delay in Milliseconds
+	delayInMS := 1000
+
+	// Call stream func using Stock object and a given delay
+	stream := plutus.Stream(stock, delayInMS)
+
+	// Get updated data and print out most recent stock price. Runs infinently and returns the newest avalible stock data in the form of a plutus.Stock struct
+	data := <-stream
+	fmt.Println(data.Price)
+}
