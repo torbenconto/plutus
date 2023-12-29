f33ffc7855be88faee729299a29b26888fe28564
pvwnthem <pvwn.them@gmail.com>
2023-12-29 04:58:48 -0500

    /stock/:ticker api route

--- api/main.go
+++ api/main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/torbenconto/plutus"
 )
 
-func setupRouter() {
+func setupRouter() *gin.Engine {
 	// Initialize gin router
 	r := gin.Default()
 
@@ -13,7 +14,38 @@ func setupRouter() {
 			"message": "pong",
 		})
 	})
+
+	r.GET("/stock/:ticker", func(c *gin.Context) {
+		// Get provider from url param
+
+		var activeProvider plutus.StockDataProvider
+		providerQuery := c.Query("provider")
+		// None specified, use default
+		if providerQuery == "" {
+			activeProvider = plutus.YahooFinanceProvider
+		}
+
+		ticker := c.Param("ticker")
+		stock, err := plutus.NewStock(ticker, activeProvider)
+		if err != nil {
+			c.JSON(404, gin.H{
+				"error": err,
+			})
+		} else {
+			c.JSON(200, stock)
+		}
+
+	})
+
+	return r
 }
 
 func main() {
+	r := setupRouter()
+
+	// Listen and serve on 8080
+	err := r.Run(":8080")
+	if err != nil {
+		return
+	}
 }
