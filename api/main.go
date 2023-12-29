2678edb040b9f384e42526563820f57abdb98f95
pvwnthem <pvwn.them@gmail.com>
2023-12-29 05:12:35 -0500

    simple changes, use http.{code} instead of number

--- api/main.go
+++ api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/torbenconto/plutus"
+	"net/http"
 )
 
 func setupRouter() *gin.Engine {
@@ -10,29 +11,33 @@ func setupRouter() *gin.Engine {
 	r := gin.Default()
 
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
 
 	r.GET("/stock/:ticker", func(c *gin.Context) {
-		// Get provider from url param
-
+		// Variable to set the current provider
 		var activeProvider plutus.StockDataProvider
+
+		// Get provider from url param
 		providerQuery := c.Query("provider")
 		// None specified, use default
 		if providerQuery == "" {
 			activeProvider = plutus.YahooFinanceProvider
 		}
 
+		// Get ticker from url param
 		ticker := c.Param("ticker")
+		// Create new stock instance
 		stock, err := plutus.NewStock(ticker, activeProvider)
+		// Check for errors, return 404 if not found or 200 along with stock data if found
 		if err != nil {
-			c.JSON(404, gin.H{
+			c.JSON(http.StatusNotFound, gin.H{
 				"error": err,
 			})
 		} else {
-			c.JSON(200, stock)
+			c.JSON(http.StatusOK, stock)
 		}
 
 	})
