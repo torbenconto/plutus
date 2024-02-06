fb76027980490e8c5d04c52797925933938fda89
torbenconto <torben@2x1.dev>
2024-02-06 18:25:57 -0500

    Add comment to BuildRequestFromConfig

--- internal/util/configs.go
+++ internal/util/configs.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 )
 
+// BuildRequestFromConfig modifies a http.Request from a config.Config. url is the un-formatted url, fallback url is the complete, formatted url.
 func BuildRequestFromConfig(req *http.Request, conf config.Config, url string, fallbackUrl string) (*http.Request, error) {
 	var err error
 
