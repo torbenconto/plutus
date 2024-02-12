3da59dd778d03b296d755e7fd10aa87e59bcca90
torbenmconto <torbenmconto@gmail.com>
2024-02-11 20:58:49 -0500

    Delete util.go

--- tests/util.go
+++
@@ -1,9 +0,0 @@
-package tests
-
-import "reflect"
-
-func GetField(s interface{}, field string) interface{} {
-	r := reflect.ValueOf(s)
-	f := reflect.Indirect(r).FieldByName(field)
-	return f.Interface()
-}
